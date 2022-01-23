9bfc58de1eed9088b439a94a9c4f49fdf42ae943
Anvesh Checka <anveshch@gmail.com>
2022-01-23 22:54:44 +0530

    cleanup of duplicate styles

--- styles/global.js
+++ styles/global.js
@@ -7,14 +7,34 @@ const GlobalStyle = createGlobalStyle`
     font-family: "Open Sans";
     font-style: normal;
     font-weight: 400;
+    font-display: swap;
+    src: local("Open Sans"), url(/fonts/OpenSans-VariableFont_wdth,wght.ttf) format('truetype');
+    text-decoration: none;
+  }
+  @font-face {
+    font-family: "Arimo";
+    font-style: normal;
+    font-weight: 400;
     font-display: block;
-    src: local("Open Sans") url(/fonts/Open_Sans/OpenSans-VariableFont_wdth,wght.ttf) format('truetype');
+    src: local("Arimo"), url(/fonts/Arimo-VariableFont_wght.ttf) format('truetype');
+    text-decoration: none;
+  }
+  @font-face {
+    font-family: "Playfair_Display";
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local("Playfair_Display"), url(/fonts/PlayfairDisplay-VariableFont_wght.ttf) format('truetype');
     text-decoration: none;
   }
   body {
     color: ${props => (props.whiteColor ? 'white' : 'black')};
     font-size: 16px;
     font-family: 'Open Sans', sans-serif;
+  }
+  nav {
+    font-size: 16px;
+    font-family: 'Playfair_Display', sans-serif;
   }
   // theme
     // fonts
