af59d5346842c88281e2e9cf02f428185b75c0a0
Anvesh Checka <anveshch@gmail.com>
2022-01-30 20:28:48 +0530

    Dentures page

--- pages/dentures.js
+++ pages/dentures.js
@@ -1,3 +1,27 @@
-export default function SingleSitting() {
-  return <h1>Dentures</h1>
+import Layout from '../components/layout';
+import { SectionGroup, SectionItem, TreatmentWrapper, TreatmentHeading, TreatmentDescription, TreatmentLogo } from '../styles/banner.styled';
+import Footer from '../components/footer';
+
+export default function Dentures() {
+  return (
+    <>
+      <Layout showDetails/>
+      <SectionGroup>
+        <SectionItem>
+          <TreatmentHeading>
+            <h1>Dentures</h1>
+          </TreatmentHeading>
+          <TreatmentWrapper>
+            <TreatmentDescription>
+              <span>Dentures are also known as “false teeth”. These are prosthetic devices that are constructed to replace the missing teeth and is attached to the gums with the hard and soft tissues. Dentures could be designed according to the necessity of the patient such as partial dentures, custom dentures, overdentures, implant dentures, etc...</span>
+            </TreatmentDescription>
+            <TreatmentLogo>
+              <img src="/dentures.jpeg" />
+            </TreatmentLogo>
+          </TreatmentWrapper>
+        </SectionItem>
+      </SectionGroup>
+      <Footer />
+    </>
+  );
 }
