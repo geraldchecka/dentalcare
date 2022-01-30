7018b4c454f3ac07534379259188bb222cf3f39f
Anvesh Checka <anveshch@gmail.com>
2022-01-30 21:06:28 +0530

    added contact us page

--- pages/contactus.js
+++ pages/contactus.js
@@ -1,10 +1,27 @@
 import Layout from '../components/layout';
+import { SectionGroup, SectionItem, TreatmentWrapper, TreatmentHeading, TreatmentDescription, TreatmentLogo } from '../styles/banner.styled';
+import Footer from '../components/footer';
 
 export default function ContactUs() {
   return (
     <>
-      <Layout />
-      <p>Contact US</p>
+      <Layout showDetails/>
+      <SectionGroup>
+        <SectionItem>
+          <TreatmentHeading>
+            <h1>Contact Us</h1>
+          </TreatmentHeading>
+          <TreatmentWrapper>
+            <TreatmentDescription>
+              <span>Location Required</span>
+            </TreatmentDescription>
+            {/* <TreatmentLogo>
+              <img src="/dental-fillings.jpeg" />
+            </TreatmentLogo> */}
+          </TreatmentWrapper>
+        </SectionItem>
+      </SectionGroup>
+      <Footer />
     </>
   );
 }
