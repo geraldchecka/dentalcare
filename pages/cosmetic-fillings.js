c612e5d37e453aa0d6b0a4a136280a4ce9c3398a
Anvesh Checka <anveshch@gmail.com>
2022-02-18 23:10:09 +0530

    cosmetic fillings image

--- pages/cosmetic-fillings.js
+++ pages/cosmetic-fillings.js
@@ -16,7 +16,7 @@ export default function CosmeticFillings() {
               <span>The cosmetic filling is a dental procedure where a filling of composite resins or silver amalgam is used to fill in the decay of the tooth so that it doesn’t spread or aggravate to the roots. There are various types of filling like tin, copper, silver, gold, zinc, and mercury. In the procedure of cosmetic fillings sometimes composite fillings do not get the proper shade of the tooth with other shades. It is then composed of acrylic resin and glass which gives a white colour that matches the teeth. Different fillings are used for different teeth conditions like teeth decaying problems such as filling new cavities, replacing metal amalgams, repairing chipped teeth, removing gaps between the teeth, and reshaping teeth.</span>
             </TreatmentDescription>
             <TreatmentLogo>
-              <img src="/cosmetic-fillings.jpeg" />
+              <img src="/cosmetic-fillings-new.jpeg" />
             </TreatmentLogo>
           </TreatmentWrapper>
         </SectionItem>
